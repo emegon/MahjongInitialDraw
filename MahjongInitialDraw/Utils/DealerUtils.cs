d4c1ed2e95fbdb65ec6dea823f3144b3f4d5e3cc
emegon <emegon.co.jp@gmail.com>
2025-01-23 21:17:27 +0900

    Add Dora

---
+++ MahjongInitialDraw/Utils/DealerUtils.cs
@@ -0,0 +1,13 @@
+﻿namespace MahjongInitialDraw.Utils;
+
+public static class DealerUtils
+{
+    private static readonly IReadOnlyList<int> TileIds = Enumerable.Range(1, 34 * 4).ToList().AsReadOnly();
+
+    public static IEnumerable<int> ShuffleTileIds()
+    {
+        var random = new Random();
+
+        return TileIds.OrderBy(x => random.Next());
+    }
+}
